c40c96c39d450278eb737c4309498bc3002f6f07
Sunny Batabyal <batabyalsunny@gmail.com>
2019-09-15 17:04:57 +0530

    Initial spring boot project setup

---
+++ src/main/java/alaap/ga/MuneemApplication.java
@@ -0,0 +1,13 @@
+package alaap.ga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuneemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuneemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alaap/ga/MuneemApplicationTests.java
@@ -0,0 +1,16 @@
+package alaap.ga;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MuneemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
